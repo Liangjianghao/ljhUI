eae026b03537a1d57dd36774ae47a46fad5e2bd6
mac <mac@macdeiMac.local>
2017-01-19 18:39:56 +0800

    基本完成

---
+++ ljhUI/ljhUI/storeList.h
@@ -0,0 +1,13 @@
+//
+//  storeList.h
+//  ljhUI
+//
+//  Created by mac on 17/1/19.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface storeList : UIViewController
+
+@end
